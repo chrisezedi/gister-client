f34e65e8575b59aafaa85ef4831ad95a75cbac3d
ekene-ezedi <ezedi.eo.ekene@gmail.com>
2020-10-11 01:38:31 +0100

    modify user component

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -7,11 +7,11 @@ import React from 'react';
                  <div className="col-md-6">
                     <div className="shadow-sm border-0">
                         <div className="d-flex justify-content-center">
-                            <img src={user.picture} className="rounded-pill" alt={user.name} width="100px" height="100px"/>
+                            {/* <img src={user.picture} className="rounded-pill" alt={user.name} width="100px" height="100px"/> */}
                         </div>
                         <div className="card-body">
-                            <h3>Name </h3> <p className="card-text">{user.name}</p>
-                            <h3>Rooms </h3> <p className="card-text">{`${user.rooms.length}/5`}</p>
+                            {/* <h3>Name </h3> <p className="card-text">{user.name}</p>
+                            <h3>Rooms </h3> <p className="card-text">{`${user.rooms.length}/5`}</p> */}
                         </div>
                     </div>
                  </div>
